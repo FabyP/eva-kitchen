9e2680f99b0c45a695307b0e3890d92f3c4359d0
unknown <fabienne.pipping@gmail.com>
2020-09-13 20:42:43 +0200

    built order screen

--- src/screens/order/Order.js
+++ src/screens/order/Order.js
@@ -1,13 +1,155 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import socketIOClient from "socket.io-client";
+import http from "../../http-common";
+
+import Box from "@material-ui/core/Box";
+import Collapse from "@material-ui/core/Collapse";
+import IconButton from "@material-ui/core/IconButton";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
+import DoneIcon from "@material-ui/icons/Done";
+import Checkbox from "@material-ui/core/Checkbox";
+
+import "./Order.css";
+import { purple } from "@material-ui/core/colors";
+
+const Row = (order) => {
+  const [open, setOpen] = useState(true);
+  const [table, setTable] = useState({});
+  const date = new Date(order.order.OrderTimeStamp);
+
+  function total(items) {
+    let sum = 0;
+    items.forEach((item) => {
+      sum += parseFloat(item.qty) * parseFloat(item.menuitemprice);
+    });
+
+    return sum.toFixed(2).toString();
+  }
+
+  function changeIconColor() {}
+
+  function setPayed(id) {
+    http
+      .patch("/order/" + id, { StatusPayed: true })
+      .then(function () {
+        console.log("payed");
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  const fetchTable = async () => {
+    http
+      .get("/tables/" + order.order.TableId)
+      .then(function (response) {
+        setTable(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    fetchTable();
+  });
+
+  return (
+    <React.Fragment>
+      <TableRow>
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {table.TableName}
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {date.getHours()}:{date.getMinutes()}
+        </TableCell>
+        <TableCell component="th" scope="row" align="right">
+          <Button
+            variant="contained"
+            className="paidBtn"
+            onClick={() => setPayed(order.order._id)}
+          >
+            Bezahlt
+          </Button>
+        </TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box margin={1}>
+              <Table size="small" aria-label="order-items">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Gericht</TableCell>
+                    <TableCell>Anzahl</TableCell>
+                    <TableCell>Preis (€)</TableCell>
+                    <TableCell align="right">Status</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {order.order.OrderItems.map((orderItem) => (
+                    <TableRow key={orderItem._id}>
+                      <TableCell component="th" scope="row">
+                        {orderItem.name}
+                      </TableCell>
+                      <TableCell>{orderItem.qty}</TableCell>
+                      <TableCell>{orderItem.menuitemprice}</TableCell>
+                      <TableCell align="right">
+                        {/* <IconButton
+                          className="tableButton"
+                          onClick={() => changeIconColor()}
+                        >
+                          <DoneIcon className="tableIcon mx-auto" />
+                        </IconButton> */}
+                        <Checkbox
+                          inputProps={{ "aria-label": "OrderItem checkbox" }}
+                        />
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                  <TableRow>
+                    <TableCell rowSpan={3} />
+                    <TableCell colSpan={2}>Gesamtpreis</TableCell>
+                    <TableCell align="right">
+                      {total(order.order.OrderItems)}€
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  );
+};
 
 const Order = () => {
   const [orders, setOrders] = useState([]);
 
   const fetchOrders = async () => {
-    await axios
-      .get("http://localhost:9000/orders")
+    await http
+      .get("/orders")
       .then(function (response) {
         setOrders(response.data);
       })
@@ -18,9 +160,9 @@ const Order = () => {
 
   useEffect(() => {
     const socket = socketIOClient("http://localhost:9000/");
-    socket.on('event', function(data){
-      if(data.order === "updated"){
-          fetchOrders();
+    socket.on("event", function (data) {
+      if (data.order === "updated") {
+        fetchOrders();
       }
     });
     fetchOrders();
@@ -28,11 +170,37 @@ const Order = () => {
 
   return (
     <div className="container">
-      {orders.map((order) => (
-        <p key={order._id}>
-          Tisch: {order.TableId}, Customer: {order.CustomerId} Items: {JSON.stringify(order.OrderItems)} 
-        </p>
-      ))}
+      <Paper>
+        <Toolbar className="tableToolbar">
+          <Typography
+            className="title"
+            variant="h6"
+            id="tableTitle"
+            component="div"
+          >
+            Bestellungen
+          </Typography>
+        </Toolbar>
+        <TableContainer>
+          <Table aria-label="collapsible table">
+            <TableHead>
+              <TableRow>
+                <TableCell />
+                <TableCell>Bestellung</TableCell>
+                <TableCell>Uhrzeit</TableCell>
+                <TableCell align="right">Bezahlt</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {orders.map((order) =>
+                order.StatusPayed === false ? (
+                  <Row key={order._id} order={order} />
+                ) : null
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
     </div>
   );
 };
